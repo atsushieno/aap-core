b78275305280e4aa5f1100933d1aedc00105ad9a
atsushieno <atsushieno@gmail.com>
2019-08-11 16:26:02 +0900

    Got AddressSanitizer working (disabled from the build, check build.gradle for enabling it).

---
+++ setup-asan-for-debugging.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This shell script is to set up ASAN for debugging native libs.
+# Specify your own path in those variables below.
+
+ANDROID_NDK_PATH=~/Android/Sdk/ndk/20.0.5594570
+HOST_ARCH_LIB=linux-x86_64/lib64
+CLANG_VER=8.0.7
+
+
+CLANG_LIB=$ANDROID_NDK_PATH/toolchains/llvm/prebuilt/$HOST_ARCH_LIB/clang/$CLANG_VER/lib
+SAMPLE=java/samples/aaphostsample/src/main
+SAMPLE_RES=$SAMPLE/resources/lib
+
+cp $CLANG_LIB/linux/libclang_rt.*-i686-android.so $SAMPLE/jniLibs/x86/
+cp $CLANG_LIB/linux/libclang_rt.*-x86_64-android.so $SAMPLE/jniLibs/x86_64/
+cp $CLANG_LIB/linux/libclang_rt.*-arm-android.so $SAMPLE/jniLibs/armeabi-v7a/
+cp $CLANG_LIB/linux/libclang_rt.*-aarch64-android.so $SAMPLE/jniLibs/arm64-v8a/
+mkdir -p $SAMPLE_RES/x86
+mkdir -p $SAMPLE_RES/x86_64
+mkdir -p $SAMPLE_RES/armeabi-v7a
+mkdir -p $SAMPLE_RES/aarch64-v8a
+cp -R $ANDROID_NDK_PATH/wrap.sh/asan.x86.sh $SAMPLE_RES/x86/
+cp -R $ANDROID_NDK_PATH/wrap.sh/asan.x86_64.sh $SAMPLE_RES/x86_64/
+cp -R $ANDROID_NDK_PATH/wrap.sh/asan.armeabi-v7a.sh $SAMPLE_RES/armeabi-v7a/
+cp -R $ANDROID_NDK_PATH/wrap.sh/asan.arm64-v8a.sh $SAMPLE_RES/aarch64-v8a/
