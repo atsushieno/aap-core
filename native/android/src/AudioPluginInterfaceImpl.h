94e91a603bcc5639430d15a0fcecb8b9fc611e6b
atsushieno <atsushieno@gmail.com>
2019-11-15 07:04:36 -0800

    clean up host native implementation code.

--- native/android/src/AudioPluginInterfaceImpl.h
+++ native/android/src/AudioPluginInterfaceImpl.h
@@ -44,7 +44,6 @@ public:
             return ndk::ScopedAStatus(
                     AStatus_fromServiceSpecificErrorWithMessage(-1, "Already instantiated"));
         instance = host->instantiatePlugin(in_pluginId.data());
-        __android_log_print(ANDROID_LOG_INFO, "!!!AAAAA!!!", "instantiated plugin at host. Plugin is remote? %d", instance->getPluginDescriptor()->isOutProcess());
         return ndk::ScopedAStatus::ok();
     }
 
@@ -61,7 +60,6 @@ public:
     }
 
     bool resetBuffers(int frameCount, const std::vector<int64_t> &newFDs) {
-        __android_log_print(ANDROID_LOG_INFO, "!!!AAPDEBUG!!!", "resetBuffers/");
         if (sharedMemoryFDs.size() != newFDs.size()) {
             freeBuffers();
             sharedMemoryFDs.resize(newFDs.size(), 0);
@@ -73,29 +71,19 @@ public:
         if (!buffer.buffers)
             buffer.buffers = (void **) calloc(sizeof(void *), n + 1);
         for (int i = 0; i < n; i++) {
-            //if (sharedMemoryFDs[i] != newFDs[i]) { // shm FD has changed
-			//	__android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "shm FD has changed. %d Size = %d", (int) newFDs[i], ASharedMemory_getSize(newFDs[i]));
-                if (buffer.buffers[i]) {
-					__android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "shm unmapping");
-					munmap(buffer.buffers[i], current_buffer_size);
-				}
-                //sharedMemoryFDs[i] = newFDs[i];
-                buffer.buffers[i] = mmap(nullptr, current_buffer_size, PROT_READ | PROT_WRITE,
-                                         MAP_SHARED, sharedMemoryFDs[i], 0);
-                if(buffer.buffers[i] == MAP_FAILED) {
-                	int err = errno;
-					__android_log_print(ANDROID_LOG_ERROR, "AndroidAudioPlugin",
-										"mmap failed at buffer %d. errno = %d", i, err);
-					return false;
-				}
-				__android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "shm %d mmapped with size %d", i, current_buffer_size);
-                assert(buffer.buffers[i] != nullptr);
-            //}
+            if (buffer.buffers[i])
+                munmap(buffer.buffers[i], current_buffer_size);
+            buffer.buffers[i] = mmap(nullptr, current_buffer_size, PROT_READ | PROT_WRITE,
+                                     MAP_SHARED, sharedMemoryFDs[i], 0);
+            if (buffer.buffers[i] == MAP_FAILED) {
+                int err = errno;
+                __android_log_print(ANDROID_LOG_ERROR, "AndroidAudioPlugin",
+                                    "mmap failed at buffer %d. errno = %d", i, err);
+                return false;
+            }
+            assert(buffer.buffers[i] != nullptr);
         }
         buffer.buffers[newFDs.size()] = nullptr;
-
-        __android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "/resetBuffers");
-
         return true;
     }
 
@@ -110,27 +98,24 @@ public:
     }
 
     ::ndk::ScopedAStatus prepare(int32_t in_frameCount, int32_t in_portCount) override {
-        __android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "prepare");
         // FIXME: error check
-        resetBuffers(in_frameCount, sharedMemoryFDs);
+        if (!resetBuffers(in_frameCount, sharedMemoryFDs)) {
+            __android_log_print(ANDROID_LOG_ERROR, "AndroidAudioPlugin", "Failed to prepare shared memory buffers.");
+            return ndk::ScopedAStatus{AStatus_fromServiceSpecificError(2)}; // FIXME : define error codes
+        }
         instance->prepare(sample_rate, in_frameCount, &buffer);
-        __android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "prepare done");
         for (int i = 0; i < sharedMemoryFDs.size(); i++) {
-			__android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "writing shm %d", i);
 			memset(buffer.buffers[i], 1, 10);
 		}
-        __android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "shm is writable. good good");
         return ndk::ScopedAStatus::ok();
     }
 
     ::ndk::ScopedAStatus activate() override {
-        __android_log_print(ANDROID_LOG_INFO, "_AAPDEBUG_", "activate");
         instance->activate();
         return ndk::ScopedAStatus::ok();
     }
 
     ::ndk::ScopedAStatus process(int32_t in_timeoutInNanoseconds) override {
-        __android_log_print(ANDROID_LOG_INFO, "!!!AAPDEBUG!!!", "process");
         instance->process(&buffer, in_timeoutInNanoseconds);
         return ndk::ScopedAStatus::ok();
     }
@@ -147,14 +132,14 @@ public:
 
     ::ndk::ScopedAStatus getState(const ::ndk::ScopedFileDescriptor& in_sharedMemoryFD) override {
         auto size = instance->getStateSize();
-        auto dst = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, in_sharedMemoryFD.get(), 0);
+        auto dst = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, dup(in_sharedMemoryFD.get()), 0);
         memcpy(dst, instance->getState(), size);
         munmap(dst, size);
         return ndk::ScopedAStatus::ok();
     }
 
     ::ndk::ScopedAStatus setState(const ::ndk::ScopedFileDescriptor& in_sharedMemoryFD, int32_t in_size) override {
-        auto src = mmap(nullptr, in_size, PROT_READ | PROT_WRITE, MAP_SHARED, in_sharedMemoryFD.get(), 0);
+        auto src = mmap(nullptr, in_size, PROT_READ | PROT_WRITE, MAP_SHARED, dup(in_sharedMemoryFD.get()), 0);
         instance->setState(src, 0, in_size);
         munmap(src, in_size);
         return ndk::ScopedAStatus::ok();
