ba3a64c4c7ffbbb6e202fa5dbc98b8092d75e2b2
atsushieno <atsushieno@gmail.com>
2019-11-15 05:26:41 -0800

    Now that those LV2 plugins assets and native libs are packaged in an aar.
    It is a smart move, now aaphostsample (for now) and localpluginsample can
    easily share the same LV2 plugins.

--- import-lv2-deps.sh
+++ import-lv2-deps.sh
@@ -4,8 +4,7 @@ ABIS_SIMPLE=(x86 x86_64 armeabi-v7a arm64-v8a)
 
 # Remove existing jniLibs for sanity
 rm -rf java/androidaudioplugin-lv2/src/main/jniLibs/
-rm -rf java/samples/aaphostsample/src/main/jniLibs/
-rm -rf java/samples/localpluginsample/src/main/jniLibs/
+rm -rf java/samples/aaplv2plugins/src/main/jniLibs/
 
 
 for abi in ${ABIS_SIMPLE[*]}
@@ -15,25 +14,21 @@ do
     mkdir -p java/androidaudioplugin-lv2/src/main/jniLibs/$abi
     cp -R dependencies/cerbero-artifacts/outputs/$abi/lib/*.so java/androidaudioplugin-lv2/src/main/jniLibs/$abi/
     # And then copy native libs of LV2 plugins for each ABI.
-    mkdir -p java/samples/aaphostsample/src/main/jniLibs/$abi
-    mkdir -p java/samples/localpluginsample/src/main/jniLibs/$abi
-    cp -R dependencies/cerbero-artifacts/outputs/$abi/lib/lv2/*/*.so java/samples/aaphostsample/src/main/jniLibs/$abi/
-    cp -R dependencies/cerbero-artifacts/outputs/$abi/lib/lv2/*/*.so java/samples/localpluginsample/src/main/jniLibs/$abi/
+    mkdir -p java/samples/aaplv2plugins/src/main/jniLibs/$abi
+    cp -R dependencies/cerbero-artifacts/outputs/$abi/lib/lv2/*/*.so java/samples/aaplv2plugins/src/main/jniLibs/$abi/
 done
 
 # Copy LV2 metadata files etc.
 # The non-native parts should be the same so we just copy files from x86 build.
-cp -R dependencies/cerbero-artifacts/outputs/x86/lib/lv2/ java/samples/aaphostsample/src/main/assets/
-cp -R dependencies/cerbero-artifacts/outputs/x86/lib/lv2/ java/samples/localpluginsample/src/main/assets/
+mkdir -p java/samples/aaplv2plugins/src/main/assets/
+cp -R dependencies/cerbero-artifacts/outputs/x86/lib/lv2/ java/samples/aaplv2plugins/src/main/assets/
 # ... except for *.so files. They are stored under jniLibs.
-rm java/samples/aaphostsample/src/main/assets/lv2/*/*.so
-rm java/samples/localpluginsample/src/main/assets/lv2/*/*.so
+rm java/samples/aaplv2plugins/src/main/assets/lv2/*/*.so
 
 # Generate `aap-metadata.xml` that AAP service look up plugins.
+mkdir -p java/samples/aaplv2plugins/src/main/assets/lv2
+mkdir -p java/samples/aaplv2plugins/src/main/res/xml
 tools/aap-import-lv2-metadata/build/aap-import-lv2-metadata \
-	java/samples/aaphostsample/src/main/assets/lv2 \
-	java/samples/aaphostsample/src/main/res/xml
-tools/aap-import-lv2-metadata/build/aap-import-lv2-metadata \
-	java/samples/localpluginsample/src/main/assets/lv2 \
-	java/samples/localpluginsample/src/main/res/xml
+	java/samples/aaplv2plugins/src/main/assets/lv2 \
+	java/samples/aaplv2plugins/src/main/res/xml
 
