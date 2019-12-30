658993055d9820156590a03018993b2ba27d3544
atsushieno <atsushieno@gmail.com>
2019-12-31 04:25:25 +0900

    fix JUCE project fixup script to give right filename (`gradle.properties`)

--- native/juce/build.sh
+++ native/juce/build.sh
@@ -4,7 +4,7 @@ $JUCE_DIR/Projucer --resave android-audio-plugin-framework.jucer
 
 # There is no way to generate those files in Projucer.
 cp project-override.build.gradle Builds/Android/build.gradle
-cp copy-into-generated-project.gradle.properties Builds/Android
+cp copy-into-generated-project.gradle.properties Builds/Android/gradle.properties
 
 cd Builds/Android && ./gradlew build
 
