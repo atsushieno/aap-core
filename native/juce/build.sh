7af649779f4853cb189c325bd347de91a0fb80d9
atsushieno <atsushieno@gmail.com>
2020-01-20 23:31:09 +0900

    [juceaap] Fixed desktop linking

--- native/juce/build.sh
+++ native/juce/build.sh
@@ -6,5 +6,8 @@ $JUCE_DIR/Projucer --resave android-audio-plugin-framework.jucer
 cp project-override.build.gradle Builds/Android/build.gradle
 cp copy-into-generated-project.gradle.properties Builds/Android/gradle.properties
 
+sed -e "s/project (\"JuceAAPAudioPluginHost\" C CXX)/project (\"JuceAAPAudioPluginHost\" C CXX)\n\nlink_directories (\n  \$\{CMAKE_CURRENT_SOURCE_DIR\}\/..\/..\/..\/..\/build\/native\/androidaudioplugin \n  \$\{CMAKE_CURRENT_SOURCE_DIR\}\/..\/..\/..\/..\/build\/native\/androidaudioplugin-lv2) \n\n/" Builds/CLion/CMakeLists.txt  > Builds/CLion/CMakeLists.txt.patched
+mv Builds/CLion/CMakeLists.txt.patched Builds/CLion/CMakeLists.txt
+
 cd Builds/Android && ./gradlew build
 
