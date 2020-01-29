d18b4b1d749daff9158b4bc562e86b4d9b345dba
atsushieno <atsushieno@gmail.com>
2020-01-29 17:08:37 +0900

    attempt to Fix bitrise JUCE app build.

--- native/juce/build.sh
+++ native/juce/build.sh
@@ -14,5 +14,7 @@ cp copy-into-generated-project.gradle.properties Builds/Android/gradle.propertie
 sed -e "s/project (\"JuceAAPAudioPluginHost\" C CXX)/project (\"JuceAAPAudioPluginHost\" C CXX)\n\nlink_directories (\n  \$\{CMAKE_CURRENT_SOURCE_DIR\}\/..\/..\/..\/..\/build\/native\/androidaudioplugin \n  \$\{CMAKE_CURRENT_SOURCE_DIR\}\/..\/..\/..\/..\/build\/native\/androidaudioplugin-lv2) \n\n/" Builds/CLion/CMakeLists.txt  > Builds/CLion/CMakeLists.txt.patched
 mv Builds/CLion/CMakeLists.txt.patched Builds/CLion/CMakeLists.txt
 
-cd Builds/Android && ./gradlew build
+cd Builds/LinuxMakefile && make && cd ../..
+
+cd Builds/Android && ./gradlew -d build && cd ../..
 
