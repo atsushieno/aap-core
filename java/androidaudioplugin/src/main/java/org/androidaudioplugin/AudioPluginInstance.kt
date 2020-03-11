c8d377afafe79721ddd8382d9f23a970d8e3872b
atsushieno <atsushieno@gmail.com>
2020-03-11 19:45:57 +0900

    add untested state getter/setter implementation.

--- java/androidaudioplugin/src/main/java/org/androidaudioplugin/AudioPluginInstance.kt
+++ java/androidaudioplugin/src/main/java/org/androidaudioplugin/AudioPluginInstance.kt
@@ -6,33 +6,64 @@ import java.nio.ByteBuffer
 
 class AudioPluginInstance(var instanceId: Int, var pluginInfo: PluginInformation, var service: AudioPluginServiceConnection)
 {
-    private var shared_memory_list = mutableListOf<SharedMemory>()
-    private var shared_memory_fd_list = mutableListOf<Int>()
-    private var shared_memory_buffer_list = mutableListOf<ByteBuffer>()
+    private class SharedMemoryBuffer (var shm : SharedMemory, var fd : Int, var buffer: ByteBuffer)
+
+    private var shm_list = mutableListOf<SharedMemoryBuffer>()
+
+    val proxy : AudioPluginInterface
+
+    init {
+        proxy = AudioPluginInterface.Stub.asInterface(service.binder!!)
+    }
 
     fun prepare(sampleRate: Int, samplesPerBlock: Int) {
-        var proxy = AudioPluginInterface.Stub.asInterface(service.binder!!)
         (0 until pluginInfo.getPortCount()).forEach { i ->
             var shm = SharedMemory.create(null, samplesPerBlock * 4)
             var shmFD = AudioPluginNatives.getSharedMemoryFD(shm)
             var buffer = shm.mapReadWrite()
-            shared_memory_list.add(shm)
-            shared_memory_fd_list.add(shmFD)
-            shared_memory_buffer_list.add(buffer)
+            shm_list.add(SharedMemoryBuffer(shm, shmFD, buffer))
             proxy.prepareMemory(instanceId, i, ParcelFileDescriptor.adoptFd(shmFD))
         }
         proxy.prepare(instanceId, samplesPerBlock, pluginInfo.ports.size)
     }
 
+    fun activate() {
+        proxy.activate(instanceId)
+    }
+
     fun process() {
-        var proxy = AudioPluginInterface.Stub.asInterface(service.binder!!)
         proxy.process(instanceId, 0)
     }
 
+    fun deactivate() {
+        proxy.deactivate(instanceId)
+    }
+
+    fun getState() : ByteArray {
+        var size = proxy.getStateSize(instanceId)
+        var shm = SharedMemory.create(null, size)
+        var shmFD = AudioPluginNatives.getSharedMemoryFD(shm)
+        proxy.getState(instanceId, ParcelFileDescriptor.adoptFd(shmFD))
+        var buffer = shm.mapReadWrite()
+        var array = ByteArray(size)
+        buffer.get(array, 0, size)
+        return array
+    }
+
+    fun setState(data: ByteArray) {
+        var size = data.size
+        var shm = SharedMemory.create(null, size)
+        var shmFD = AudioPluginNatives.getSharedMemoryFD(shm)
+        var buffer = shm.mapReadWrite()
+        buffer.put(buffer)
+        proxy.setState(instanceId, ParcelFileDescriptor.adoptFd(shmFD), size)
+    }
+
     fun destroy() {
-        var proxy = AudioPluginInterface.Stub.asInterface(service.binder!!)
         proxy.destroy(instanceId)
-        shared_memory_fd_list.forEach { i -> AudioPluginNatives.closeSharedMemoryFD(i) }
-        shared_memory_list.forEach { shm -> shm.close() }
+        shm_list.forEach { shm ->
+            AudioPluginNatives.closeSharedMemoryFD(shm.fd)
+            shm.shm.close()
+        }
     }
 }
