d05c62ab8fe22071d91c317c4f76f7da9b495ac6
atsushieno <atsushieno@gmail.com>
2021-03-03 16:56:34 +0900

    [aapbarebonesample] Make it slightly more featureful for better dogfooding.

--- java/samples/aapbarebonepluginsample/src/main/jni/aapbarebonepluginsample.cpp
+++ java/samples/aapbarebonepluginsample/src/main/jni/aapbarebonepluginsample.cpp
@@ -5,59 +5,78 @@
 extern "C" {
 
 void sample_plugin_delete(
-		AndroidAudioPluginFactory *pluginFactory,
-		AndroidAudioPlugin *instance) {
-	delete instance;
+        AndroidAudioPluginFactory *pluginFactory,
+        AndroidAudioPlugin *instance) {
+    delete instance;
 }
 
 void sample_plugin_prepare(AndroidAudioPlugin *plugin, AndroidAudioPluginBuffer *buffer) {
-	/* do something */
+    /* do something */
 }
 
 void sample_plugin_activate(AndroidAudioPlugin *plugin) {}
 
 void sample_plugin_process(AndroidAudioPlugin *plugin,
-						   AndroidAudioPluginBuffer *buffer,
-						   long timeoutInNanoseconds) {
-	/* do anything. In this example, inputs (0,1) are directly copied to outputs (2,3) */
-	int size = buffer->num_frames * sizeof(float);
-	memcpy(buffer->buffers[2], buffer->buffers[0], size);
-	memcpy(buffer->buffers[3], buffer->buffers[1], size);
-	// skip dummy parameter (buffers[4])
+                           AndroidAudioPluginBuffer *buffer,
+                           long timeoutInNanoseconds) {
+    /* do anything. In this example, inputs (0,1) are directly copied to outputs (2,3) */
+    int size = buffer->num_frames * sizeof(float);
+
+    auto modL = ((float *) buffer->buffers[4])[0];
+    auto modR = ((float *) buffer->buffers[5])[0];
+    auto delayL = (int) ((float *) buffer->buffers[6])[0];
+    auto delayR = (int) ((float *) buffer->buffers[7])[0];
+    auto fIL = (float *) buffer->buffers[0];
+    auto fIR = (float *) buffer->buffers[1];
+    auto fOL = (float *) buffer->buffers[2];
+    auto fOR = (float *) buffer->buffers[3];
+    int delay = 512;
+    for (int i = 0; i < size / sizeof(float); i++) {
+        if (i >= delayL)
+            fOL[i] = (float) (fIL[i - delayL] * modL);
+        if (i >= delayR)
+            fOR[i] = (float) (fIR[i - delayR] * modR);
+    }
+    /*
+    memcpy(buffer->buffers[2], buffer->buffers[0], size);
+    memcpy(buffer->buffers[3], buffer->buffers[1], size);
+     */
+
+    // skip dummy parameter (buffers[4])
 }
 
 void sample_plugin_deactivate(AndroidAudioPlugin *plugin) {}
 
-void sample_plugin_get_state(AndroidAudioPlugin *plugin, AndroidAudioPluginState* state) {
-	// FIXME: implement
+void sample_plugin_get_state(AndroidAudioPlugin *plugin, AndroidAudioPluginState *state) {
+    // FIXME: implement
 }
 
 void sample_plugin_set_state(AndroidAudioPlugin *plugin, AndroidAudioPluginState *input) {
-	// FIXME: implement
+    // FIXME: implement
 }
 
 typedef struct {
-	/* any kind of extension information, which will be passed as void* */
+    /* any kind of extension information, which will be passed as void* */
 } SamplePluginSpecific;
 
 AndroidAudioPlugin *sample_plugin_new(
-		AndroidAudioPluginFactory *pluginFactory,
-		const char *pluginUniqueId,
-		int sampleRate,
-		AndroidAudioPluginExtension** extensions) {
-	return new AndroidAudioPlugin{
-			new SamplePluginSpecific{},
-			sample_plugin_prepare,
-			sample_plugin_activate,
-			sample_plugin_process,
-			sample_plugin_deactivate,
-			sample_plugin_get_state,
-			sample_plugin_set_state
-	};
+        AndroidAudioPluginFactory *pluginFactory,
+        const char *pluginUniqueId,
+        int sampleRate,
+        AndroidAudioPluginExtension **extensions) {
+    return new AndroidAudioPlugin{
+            new SamplePluginSpecific{},
+            sample_plugin_prepare,
+            sample_plugin_activate,
+            sample_plugin_process,
+            sample_plugin_deactivate,
+            sample_plugin_get_state,
+            sample_plugin_set_state
+    };
 }
 
 AndroidAudioPluginFactory *GetAndroidAudioPluginFactory() {
-	return new AndroidAudioPluginFactory{sample_plugin_new, sample_plugin_delete};
+    return new AndroidAudioPluginFactory{sample_plugin_new, sample_plugin_delete};
 }
 
 }
