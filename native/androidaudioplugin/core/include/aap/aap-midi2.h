7c9ceeab558b1f69505485260a8f736dd21cafac
atsushieno <atsushieno@gmail.com>
2021-04-23 16:40:22 +0900

    Add aap-midi2.h for MIDI 2.0 / CI extension.
    It will be used by aap-lv2 and aap-midi-device-service (under development).

---
+++ native/androidaudioplugin/core/include/aap/aap-midi2.h
@@ -0,0 +1,17 @@
+
+#if !ANDROIDAUDIOPLUGIN_MIDI2_EXTENSION_H_INCLUDED
+#define ANDROIDAUDIOPLUGIN_MIDI2_EXTENSION_H_INCLUDED 1
+namespace aap {
+
+#define AAP_MIDI_CI_EXTENSION_URI "urn://androidaudioplugin.org/extensions/midi-ci/v1"
+
+class MidiCIExtension {
+public:
+    int32_t protocol{0}; // 0: MIDI1 / 1: MIDI2
+    int32_t protocolVersion{0}; // MIDI1 = 0, MIDI2_V1 = 0
+};
+
+
+} // namespace aap
+
+#endif // ANDROIDAUDIOPLUGIN_MIDI2_EXTENSION_H_INCLUDED
