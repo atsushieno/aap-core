6ecaaf38c8b5c6e6ce27e1d50d79ecac6215dee5
atsushieno <atsushieno@gmail.com>
2021-06-03 14:51:39 +0900

    Finally upgrade to Kotlin 1.5 now that Compose works with it.
    Navigation compose needs some tweaks in implementation (URL escaping).

--- java/androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/PluginListApp.kt
+++ java/androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/PluginListApp.kt
@@ -1,16 +1,14 @@
 package org.androidaudioplugin.ui.compose
 
+import android.net.Uri
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigate
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.compose.navArgument
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 
 @Composable
 fun PluginListApp() {
@@ -29,15 +27,15 @@ fun PluginListAppContent() {
                     topBar = { TopAppBar(title = { Text(text = "Plugins in this Plugin Service") }) },
                     content = {
                         AvailablePlugins(onItemClick = { p ->
-                            navController.navigate("plugin_details/" + p.pluginId)
+                            navController.navigate("plugin_details/" + Uri.encode(p.pluginId))
                         }, pluginServices = state.availablePluginServices)
                     }
                 )
             }
 
             composable("plugin_details/{pluginId}",
-                arguments = listOf(navArgument("pluginId", {}))) { navBackStackEntry ->
-                val pluginId = navBackStackEntry.arguments?.getString("pluginId")
+                arguments = listOf(navArgument("pluginId") { type = NavType.StringType })) {
+                val pluginId = it.arguments?.getString("pluginId")
                 if (pluginId != null) {
                     val plugin = state.availablePluginServices.flatMap { s -> s.plugins }
                         .firstOrNull { p -> p.pluginId == pluginId }
