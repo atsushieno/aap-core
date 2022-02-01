3115a89126d35483c15413130ae35d9e7ae358ab
atsushieno <atsushieno@gmail.com>
2022-02-02 00:39:59 +0900

    [mididevice] more Android Stub PAL implementation that runs loop.
    Now it works more like oboe callback, for debugging callback process.

--- java/androidaudioplugin-midi-device-service/src/main/cpp/AAPMidiProcessor_android.h
+++ java/androidaudioplugin-midi-device-service/src/main/cpp/AAPMidiProcessor_android.h
@@ -2,6 +2,7 @@
 #ifndef AAP_MIDI_DEVICE_SERVICE_AAPMIDIPROCESSOR_ANDROID_H
 #define AAP_MIDI_DEVICE_SERVICE_AAPMIDIPROCESSOR_ANDROID_H
 
+#include <thread> // NDK has no jthread...
 #include <android/sharedmem.h>
 #include <oboe/Oboe.h>
 #include "AAPMidiProcessor.h"
@@ -19,18 +20,46 @@ public:
 };
 
 class AAPMidiProcessorAndroidStubPAL : public AAPMidiProcessorAndroidPAL {
+    AAPMidiProcessor *owner;
+    bool alive{true};
+    void* audioData;
+
+    std::unique_ptr<std::thread> callback_thread{nullptr};
+    void runStreamingLoop() {
+        while (alive) {
+            sleep(1);
+            owner->onAudioReady(audioData, 1024);
+        }
+    }
 
 public:
-    AAPMidiProcessorAndroidStubPAL(AAPMidiProcessor *owner) : AAPMidiProcessorAndroidPAL() {}
+    AAPMidiProcessorAndroidStubPAL(AAPMidiProcessor *owner)
+        : AAPMidiProcessorAndroidPAL(), owner(owner) {
+        audioData = calloc(sizeof(float), 1024 * 2);
+    }
+
+    ~AAPMidiProcessorAndroidStubPAL() {
+        if (audioData)
+            free(audioData);
+    }
 
     int32_t setupStream() override { return 0; }
-    int32_t startStreaming() override { return 0; }
-    int32_t stopStreaming() override { return 0; }
+    int32_t startStreaming() override {
+        assert(!callback_thread);
+        callback_thread = std::make_unique<std::thread>(static_cast<void(AAPMidiProcessorAndroidStubPAL::*)()>(&AAPMidiProcessorAndroidStubPAL::runStreamingLoop), this);
+        return 0;
+    }
+    int32_t stopStreaming() override {
+        assert(callback_thread);
+        alive = false;
+        callback_thread->join();
+        return 0;
+    }
 
     inline void setBufferCapacityInFrames(int32_t size) override {}
     inline void setFramesPerDataCallback(int32_t size) override {}
     void midiInputReceived(uint8_t* bytes, size_t offset, size_t length, int64_t timestampInNanoseconds) override {
-        aap::aprintf("AAPMidiProcessorAndroidStubPAL::midiInputReceived(timestampInNanoseconds: %d)", timestampInNanoseconds);
+aap::aprintf("AAPMidiProcessorAndroidStubPAL::midiInputReceived(timestampInNanoseconds: %d)", timestampInNanoseconds);
         for (size_t i = offset; i < offset + length; i++)
             aap::aprintf("  %x", bytes[i]);
     }
