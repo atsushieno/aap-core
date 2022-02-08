274653e0d4544023f46c24f6f460c802ba228a44
atsushieno <atsushieno@gmail.com>
2022-02-09 01:11:29 +0900

    temporarily? go back to AGP Chipmunk.
    I moved to Dolphin because the AGP alpha project could still open in beta
    in the past. Turned out it is causing loader error on Chipmunk, so Dolphin
    turned out not to be feasible yet.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
         maven ("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.3.0-alpha01")
+        classpath ("com.android.tools.build:gradle:7.2.0-beta01")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("org.jetbrains.dokka:dokka-gradle-plugin:$dokka_version")
         classpath ("org.jetbrains.compose:compose-gradle-plugin:1.0.1")
