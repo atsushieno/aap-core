e447abac9cfbab0ffe3cc2678804706486ce0c63
atsushieno <atsushieno@gmail.com>
2022-04-06 01:38:43 +0900

    anonymize those tag types in aap_midi2.h, they bring in extra public types.

--- include/aap/unstable/aap-midi2.h
+++ include/aap/unstable/aap-midi2.h
@@ -11,15 +11,14 @@ extern "C" {
 #define AAP_PROTOCOL_MIDI1_0 1
 #define AAP_PROTOCOL_MIDI2_0 2
 
-typedef struct MidiCIExtension_tag {
-public:
+typedef struct {
     int32_t protocol{0}; // 0: Unspecified / 1: MIDI1 / 2: MIDI2
     int32_t protocolVersion{0}; // MIDI1 = 0, MIDI2_V1 = 0
 } MidiCIExtension;
 
-typedef struct AAPMidiBufferHeader_tag {
-    int32_t time_options;
-    uint32_t length;
+typedef struct {
+    int32_t time_options{0};
+    uint32_t length{0};
     uint32_t reserved[6];
 } AAPMidiBufferHeader;
 
