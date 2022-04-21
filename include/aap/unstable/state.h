ee4a3ad850c58b57fe831cd40a7f1bd1fa20b47e
atsushieno <atsushieno@gmail.com>
2022-04-22 02:14:21 +0900

    update "state as an extension" header (still no service implementation).

--- include/aap/unstable/state.h
+++ include/aap/unstable/state.h
@@ -4,6 +4,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include "aap-core.h"
 #include <stdint.h>
 
 #define AAP_STATE_EXTENSION_URI "urn://androidaudioplugin.org/extensions/state/v1"
@@ -13,9 +15,18 @@ typedef struct {
     int32_t data_size;
 } aap_state_t;
 
-typedef struct {
-    void get_state(aap_state_t *result);
-    void set_state(aap_state_t *state);
+typedef struct aap_state_context_t {
+    void *context;
+    AndroidAudioPlugin* plugin;
+} aap_state_context_t;
+
+typedef int32_t (*state_extension_get_state_t) (aap_state_context_t* context, aap_state_t* destination);
+typedef int32_t (*state_extension_set_state_t) (aap_state_context_t* context, aap_state_t* source);
+
+typedef struct aap_state_extension_t {
+    void *context;
+    state_extension_get_state_t get_state;
+    state_extension_set_state_t set_state;
 } aap_state_extension_t;
 
 #ifdef __cplusplus
@@ -23,6 +34,3 @@ typedef struct {
 #endif
 
 #endif /* AAP_STATE_H_INCLUDED */
-
-
-
