726201c52be74ea7beec3cff355d64a93d2df8c9
atsushieno <atsushieno@gmail.com>
2022-04-22 22:46:28 +0900

    bump AGP to D-alpha09. It somehow broke AGP managed test device settings.

--- androidaudioplugin/build.gradle.kts
+++ androidaudioplugin/build.gradle.kts
@@ -88,7 +88,7 @@ android {
                 device = "Pixel 5"
                 apiLevel = 30
                 systemImageSource = "aosp-atd"
-                abi = "x86"
+                //abi = "x86"
             }
         }
     }
