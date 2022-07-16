91fcd47400a715409cda220c9896348dc884bfe9
atsushieno <atsushieno@gmail.com>
2022-07-16 21:41:34 +0900

    [aaphostsample] access to Compose states causes runtime crash before onCreate().

--- samples/aaphostsample/src/main/java/org/androidaudioplugin/aaphostsample/MainActivity.kt
+++ samples/aaphostsample/src/main/java/org/androidaudioplugin/aaphostsample/MainActivity.kt
@@ -1,12 +1,14 @@
 package org.androidaudioplugin.aaphostsample
 
+import android.os.Bundle
 import org.androidaudioplugin.ui.compose.PluginListActivity
 import org.androidaudioplugin.*
 
 class MainActivity : PluginListActivity() {
     override fun shouldListPlugin(info: PluginServiceInformation) = true
 
-    init {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         viewModel.topAppBarText = "Plugins on this device"
     }
 }
