ec1ee8738c74a1c79129aff2a1e6267a25c2a5f0
atsushieno <atsushieno@gmail.com>
2022-08-09 18:32:14 +0800

    start 0.7.4. bump compose version to 1.2.0 (stable).

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@
 buildscript {
     val kotlin_version: String by extra("1.7.0")
     val dokka_version: String by extra("1.7.0")
-    val compose_version: String by extra("1.1.1")
-    val aap_version: String by extra("0.7.3")
+    val compose_version: String by extra("1.2.0")
+    val aap_version: String by extra("0.7.4")
     val enable_asan: Boolean by extra(false)
 
     repositories {
@@ -13,10 +13,9 @@ buildscript {
         maven ("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:7.2.1")
+        classpath ("com.android.tools.build:gradle:7.2.2")
         classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath ("org.jetbrains.dokka:dokka-gradle-plugin:$dokka_version")
-        classpath ("org.jetbrains.compose:compose-gradle-plugin:$compose_version")
     }
 }
 
