94919b91b4bc272f066f9a3c499c848e57815837
atsushieno <atsushieno@gmail.com>
2022-10-15 12:04:09 +0900

    WaveformDrawable: clip drawing to appropriate region. It was overflown before.

--- androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/WaveformDrawable.kt
+++ androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/WaveformDrawable.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import java.nio.ByteBuffer
@@ -41,41 +42,43 @@ fun WaveformDrawable(waveData: ByteArray, waveformHeight : Dp = 64.dp) {
     Canvas(modifier = Modifier
         .fillMaxWidth()
         .height(waveformHeight)
-        .border(width = 1.dp, color = Color.Gray), onDraw = {
-        val width = this.size.width.toInt()
-        val height = this.size.height
+        .border(width = 1.dp, color = Color.Gray)) {
+        clipRect {
+            val width = this.size.width.toInt()
+            val height = this.size.height
 
-        val visualizationData =
-            getSampleVisualizationData(floatBuffer, waveData.size / 4, width).toList()
-        var vMin = Float.MAX_VALUE
-        var vMax = -Float.MIN_VALUE
-        for (pair in visualizationData) {
-            val l = pair.first
-            if (vMax < l)
-                vMax = l
-            val r = pair.second
-            if (vMin > r)
-                vMin = r
-        }
+            val visualizationData =
+                getSampleVisualizationData(floatBuffer, waveData.size / 4, width).toList()
+            var vMin = Float.MAX_VALUE
+            var vMax = -Float.MIN_VALUE
+            for (pair in visualizationData) {
+                val l = pair.first
+                if (vMax < l)
+                    vMax = l
+                val r = pair.second
+                if (vMin > r)
+                    vMin = r
+            }
 
-        for (wp in visualizationData.indices) {
-            var i = wp * 2
-            val pair = visualizationData[wp]
-            val frL = pair.first / vMax
-            val hL = frL * height / 2
-            drawLine(
-                Color.Black,
-                Offset(i.toFloat(), height / 2),
-                Offset((i + 1).toFloat(), height / 2 - hL)
-            )
-            i = wp * 2 + 1
-            val frR = pair.second / vMin
-            val hR = frR * height / 2
-            drawLine(
-                Color.Black,
-                Offset(i.toFloat(), height / 2),
-                Offset((i + 1).toFloat(), height / 2 + hR)
-            )
+            for (wp in visualizationData.indices) {
+                var i = wp * 2
+                val pair = visualizationData[wp]
+                val frL = pair.first / vMax
+                val hL = frL * height / 2
+                drawLine(
+                    Color.Black,
+                    Offset(i.toFloat(), height / 2),
+                    Offset((i + 1).toFloat(), height / 2 - hL)
+                )
+                i = wp * 2 + 1
+                val frR = pair.second / vMin
+                val hR = frR * height / 2
+                drawLine(
+                    Color.Black,
+                    Offset(i.toFloat(), height / 2),
+                    Offset((i + 1).toFloat(), height / 2 + hR)
+                )
+            }
         }
-    })
+    }
 }
