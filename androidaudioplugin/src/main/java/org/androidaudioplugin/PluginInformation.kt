6a7e921a4d409e29ab34e6e4a250562d914a81de
atsushieno <atsushieno@gmail.com>
2022-10-16 22:56:28 +0900

    deprecate "backend" property from AAP metadata. It is of no use after all.

--- androidaudioplugin/src/main/java/org/androidaudioplugin/PluginInformation.kt
+++ androidaudioplugin/src/main/java/org/androidaudioplugin/PluginInformation.kt
@@ -4,7 +4,7 @@ package org.androidaudioplugin
  * Plugin information structure. The members mostly correspond to attributes and content elements
  * in a `<plugin>` element in `aap_metadata.xml`.
  */
-class PluginInformation(var packageName: String, var localName: String, var displayName: String, var backend: String?, var version: String?,
+class PluginInformation(var packageName: String, var localName: String, var displayName: String, @Deprecated("This will go away from aap metadata") var backend: String?, var version: String?,
                         var category: String?, var author: String?, var manufacturer: String?,
                         var pluginId: String?, var sharedLibraryName: String?,
                         var libraryEntryPoint: String?, var assets: String?, var uiActivity : String?, var uiWeb : String?, var isOutProcess: Boolean)
