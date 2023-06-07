7abe27cbc9683957c7c67dca1778a14c3c152b26
Atsushi Eno <atsushieno@gmail.com>
2023-06-08 01:17:55 +0900

    bump compose-audio-controls and add some controls on DiatonicKeyboard.

--- androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/ComposeAudioPluginViewFactory.kt
+++ androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/ComposeAudioPluginViewFactory.kt
@@ -8,12 +8,7 @@ import android.widget.LinearLayout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
--- androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/ComposePluginView.kt
+++ androidaudioplugin-ui-compose/src/main/java/org/androidaudioplugin/ui/compose/ComposePluginView.kt
@@ -2,6 +2,8 @@ package org.androidaudioplugin.ui.compose
 
 import android.content.Context
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,12 +19,17 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Badge
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,6 +44,8 @@ import org.androidaudioplugin.PluginInformation
 import org.androidaudioplugin.PortInformation
 import org.androidaudioplugin.composeaudiocontrols.DefaultKnobTooltip
 import org.androidaudioplugin.composeaudiocontrols.DiatonicKeyboard
+import org.androidaudioplugin.composeaudiocontrols.DiatonicKeyboardMoveAction
+import org.androidaudioplugin.composeaudiocontrols.DiatonicKeyboardWithControllers
 import org.androidaudioplugin.composeaudiocontrols.ImageStripKnob
 
 interface PluginViewScope {
@@ -115,9 +124,27 @@ fun PluginViewScope.PluginView(
     onNoteOn: (Int, Long) -> Unit,
     onNoteOff: (Int, Long) -> Unit) {
 
-    // FIXME: there will be breaking change in DiatonicKeyboard: it will take List<Long>, not List<Int>.
-    val noteOnStates = remember { List(128) { 0 }.toMutableStateList() }
+    // Keyboard controllers
+    // This looks hacky, but it is done for compact yet touchable UI parts.
+    val noteOnStates = remember { List(128) { 0L }.toMutableStateList() }
+    var exprMode by remember { mutableStateOf(false) }
+    var octave by remember { mutableStateOf(4) }
+    Row {
+        Row(Modifier.clickable { exprMode = !exprMode }) {
+            Text("[", fontSize = 18.sp)
+            Text(if (exprMode) "\u2714" else "", Modifier.width(20.dp), fontSize = 18.sp)
+            Text("] PerNote Expr.", fontSize = 18.sp)
+        }
+        Text(" ", Modifier.width(60.dp))
+        Text("[\u25C0]", fontSize = 18.sp, modifier = Modifier.clickable { if (octave > 0) octave-- })
+        Text("Octave: $octave", fontSize = 18.sp)
+        Text("[\u25B6]", fontSize = 18.sp, modifier = Modifier.clickable { if (octave < 9) octave++ })
+    }
+
     DiatonicKeyboard(noteOnStates.toList(),
+        modifier = modifier,
+        octaveZeroBased = octave,
+        moveAction = if (exprMode) DiatonicKeyboardMoveAction.NoteExpression else DiatonicKeyboardMoveAction.NoteChange,
         // you will also insert actual musical operations within these lambdas
         onNoteOn = { note, _ ->
             noteOnStates[note] = 1
@@ -128,6 +155,8 @@ fun PluginViewScope.PluginView(
             onNoteOff(note, 0)
         }
     )
+
+    // parameter list
     LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.height(300.dp)) {
         items(parameterCount) { index ->
             val para = getParameter(index)
@@ -149,6 +178,7 @@ fun PluginViewScope.PluginView(
             }
         }
     }
+    
     /* FIXME: we'd like to show them too, but the space is too limited.
     LazyColumn(modifier.height(100.dp)) {
         items(portCount) { index ->
