f34d38f51c6c17b3aeea6ef461469a779cac6b01
Atsushi Eno <atsushieno@gmail.com>
2023-09-22 21:03:29 +0900

    shared-memory-store.h: cosmetic code reformatting.

--- include/aap/core/host/shared-memory-store.h
+++ include/aap/core/host/shared-memory-store.h
@@ -91,129 +91,129 @@ namespace aap {
         }
     };
 
-class PluginSharedMemoryStore {
-protected:
-    /*
-     * Memory allocation and mmap-ing strategy differ between client and service.
-     */
-    enum PluginBufferOrigin {
-        PLUGIN_BUFFER_ORIGIN_UNALLOCATED,
-        PLUGIN_BUFFER_ORIGIN_LOCAL,
-        PLUGIN_BUFFER_ORIGIN_REMOTE
-    };
+    class PluginSharedMemoryStore {
+    protected:
+        /*
+         * Memory allocation and mmap-ing strategy differ between client and service.
+         */
+        enum PluginBufferOrigin {
+            PLUGIN_BUFFER_ORIGIN_UNALLOCATED,
+            PLUGIN_BUFFER_ORIGIN_LOCAL,
+            PLUGIN_BUFFER_ORIGIN_REMOTE
+        };
 
-    PluginBufferOrigin memory_origin{PLUGIN_BUFFER_ORIGIN_UNALLOCATED};
+        PluginBufferOrigin memory_origin{PLUGIN_BUFFER_ORIGIN_UNALLOCATED};
 
-    // They are created by client-as-plugin.
-    std::unique_ptr<std::vector<int32_t>> extension_fds{nullptr};
-    std::unique_ptr<std::vector<void*>> extension_buffers{nullptr};
-    std::unique_ptr<std::vector<int32_t>> extension_buffer_sizes{nullptr};
+        // They are created by client-as-plugin.
+        std::unique_ptr<std::vector<int32_t>> extension_fds{nullptr};
+        std::unique_ptr<std::vector<void*>> extension_buffers{nullptr};
+        std::unique_ptr<std::vector<int32_t>> extension_buffer_sizes{nullptr};
 
-    // This is a temporary FD store for plugin services.
-    // The unknown number of calls of AIDL prepareMemory() precedes prepare(), so we have to
-    // first store those FDs somewhere.
-    // Within the AIDL, we first receive unknown number of shm FDs.
-    std::unique_ptr<std::vector<int32_t>> cached_shm_fds_for_prepare{nullptr};
+        // This is a temporary FD store for plugin services.
+        // The unknown number of calls of AIDL prepareMemory() precedes prepare(), so we have to
+        // first store those FDs somewhere.
+        // Within the AIDL, we first receive unknown number of shm FDs.
+        std::unique_ptr<std::vector<int32_t>> cached_shm_fds_for_prepare{nullptr};
 
-    // ex-PluginSharedMemoryBuffer members
-    std::unique_ptr<std::vector<int32_t>> port_buffer_fds{nullptr};
+        // ex-PluginSharedMemoryBuffer members
+        std::unique_ptr<std::vector<int32_t>> port_buffer_fds{nullptr};
 
-    // When shms are locally allocated (PLUGIN_BUFFER_ORIGIN_LOCAL), then those buffers are locally calloc()-ed.
-    // Otherwise they are just mmap()-ed and should not be freed by own.
-    std::unique_ptr<SharedMemoryPluginBuffer> port_buffer{nullptr};
-public:
-    enum PluginMemoryAllocatorResult {
-        PLUGIN_MEMORY_ALLOCATOR_SUCCESS,
-        PLUGIN_MEMORY_ALLOCATOR_FAILED_LOCAL_ALLOC,
-        PLUGIN_MEMORY_ALLOCATOR_FAILED_SHM_CREATE,
-        PLUGIN_MEMORY_ALLOCATOR_FAILED_MMAP
-    };
+        // When shms are locally allocated (PLUGIN_BUFFER_ORIGIN_LOCAL), then those buffers are locally calloc()-ed.
+        // Otherwise they are just mmap()-ed and should not be freed by own.
+        std::unique_ptr<SharedMemoryPluginBuffer> port_buffer{nullptr};
+    public:
+        enum PluginMemoryAllocatorResult {
+            PLUGIN_MEMORY_ALLOCATOR_SUCCESS,
+            PLUGIN_MEMORY_ALLOCATOR_FAILED_LOCAL_ALLOC,
+            PLUGIN_MEMORY_ALLOCATOR_FAILED_SHM_CREATE,
+            PLUGIN_MEMORY_ALLOCATOR_FAILED_MMAP
+        };
 
-    static const char* getMemoryAllocationErrorMessage(int32_t code) {
-        switch (code) {
-            case aap::PluginSharedMemoryStore::PluginMemoryAllocatorResult::PLUGIN_MEMORY_ALLOCATOR_FAILED_LOCAL_ALLOC:
-                return "Plugin client failed at allocating memory.";
-            case aap::PluginSharedMemoryStore::PluginMemoryAllocatorResult::PLUGIN_MEMORY_ALLOCATOR_FAILED_SHM_CREATE:
-                return "Plugin client failed at creating shm.";
-            case aap::PluginSharedMemoryStore::PluginMemoryAllocatorResult::PLUGIN_MEMORY_ALLOCATOR_FAILED_MMAP:
-                return "Plugin client failed at mmap.";
-            default:
-                return nullptr;
+        static const char* getMemoryAllocationErrorMessage(int32_t code) {
+            switch (code) {
+                case PLUGIN_MEMORY_ALLOCATOR_FAILED_LOCAL_ALLOC:
+                    return "Plugin client failed at allocating memory.";
+                case PLUGIN_MEMORY_ALLOCATOR_FAILED_SHM_CREATE:
+                    return "Plugin client failed at creating shm.";
+                case PLUGIN_MEMORY_ALLOCATOR_FAILED_MMAP:
+                    return "Plugin client failed at mmap.";
+                default:
+                    return nullptr;
+            }
         }
-    }
 
-    PluginSharedMemoryStore() {
-        // They are all added only via addExtensionFD().
-        // Buffers are only mmap()-ed and should always be munmap()-ed at destructor.
-        extension_fds = std::make_unique<std::vector<int32_t>>();
-        extension_buffers = std::make_unique<std::vector<void*>>();
-        extension_buffer_sizes = std::make_unique<std::vector<int32_t>>();
-        cached_shm_fds_for_prepare = std::make_unique<std::vector<int32_t>>();
+        PluginSharedMemoryStore() {
+            // They are all added only via addExtensionFD().
+            // Buffers are only mmap()-ed and should always be munmap()-ed at destructor.
+            extension_fds = std::make_unique<std::vector<int32_t>>();
+            extension_buffers = std::make_unique<std::vector<void*>>();
+            extension_buffer_sizes = std::make_unique<std::vector<int32_t>>();
+            cached_shm_fds_for_prepare = std::make_unique<std::vector<int32_t>>();
 
-        port_buffer_fds = std::make_unique<std::vector<int32_t>>();
-        assert(port_buffer_fds);
-    }
+            port_buffer_fds = std::make_unique<std::vector<int32_t>>();
+            assert(port_buffer_fds);
+        }
 
-    virtual ~PluginSharedMemoryStore() {
-        disposeExtensionFDs();
-        disposeAudioBufferFDs();
-    }
+        virtual ~PluginSharedMemoryStore() {
+            disposeExtensionFDs();
+            disposeAudioBufferFDs();
+        }
 
-    void disposeExtensionFDs() {
-        for (int i = 0; i < extension_fds->size(); i++) {
-            if (extension_buffers->at(i))
-                munmap(extension_buffers->at(i), (size_t) extension_buffer_sizes->at(i));
+        void disposeExtensionFDs() {
+            for (int i = 0; i < extension_fds->size(); i++) {
+                if (extension_buffers->at(i))
+                    munmap(extension_buffers->at(i), (size_t) extension_buffer_sizes->at(i));
+                // close the fd. AudioPluginService also dup()-s it, so it has to be closed too.
+                auto fd = extension_fds->at(i);
+                if (fd >= 0)
+                    close(fd);
+            }
+            extension_fds->clear();
+        }
+
+        void disposeAudioBufferFDs() {
+            // ex-PluginSharedMemoryBuffer part
+            if (port_buffer)
+                port_buffer->unmapSharedMemory();
             // close the fd. AudioPluginService also dup()-s it, so it has to be closed too.
-            auto fd = extension_fds->at(i);
-            if (fd >= 0)
-                close(fd);
+            for (size_t i = 0; i < port_buffer_fds->size(); i++) {
+                auto fd = port_buffer_fds->at(i);
+                if (fd >= 0)
+                    close(fd);
+            }
+            port_buffer_fds->clear();
         }
-        extension_fds->clear();
-    }
 
-    void disposeAudioBufferFDs() {
-        // ex-PluginSharedMemoryBuffer part
-        if (port_buffer)
-            port_buffer->unmapSharedMemory();
-        // close the fd. AudioPluginService also dup()-s it, so it has to be closed too.
-        for (size_t i = 0; i < port_buffer_fds->size(); i++) {
-            auto fd = port_buffer_fds->at(i);
-            if (fd >= 0)
-                close(fd);
+        // Stores clone of port buffer FDs passed from client via Binder.
+        inline void resizePortBufferByCount(size_t newSize) {
+            cached_shm_fds_for_prepare->resize(newSize);
         }
-        port_buffer_fds->clear();
-    }
 
-    // Stores clone of port buffer FDs passed from client via Binder.
-    inline void resizePortBufferByCount(size_t newSize) {
-        cached_shm_fds_for_prepare->resize(newSize);
-    }
+        // used by AudioPluginInterfaceImpl.
+        inline int32_t getPortBufferFD(size_t index) { return port_buffer_fds->at(index); }
 
-    // used by AudioPluginInterfaceImpl.
-    inline int32_t getPortBufferFD(size_t index) { return port_buffer_fds->at(index); }
+        // called by AudioPluginInterfaceImpl::prepareMemory().
+        // `fd` is an already-duplicated FD.`
+        inline void setPortBufferFD(size_t index, int32_t fd) {
+            cached_shm_fds_for_prepare->at(index) = fd;
+        }
 
-    // called by AudioPluginInterfaceImpl::prepareMemory().
-    // `fd` is an already-duplicated FD.`
-    inline void setPortBufferFD(size_t index, int32_t fd) {
-        cached_shm_fds_for_prepare->at(index) = fd;
-    }
+        void* addExtensionFD(int fd, int dataSize) {
+            extension_fds->emplace_back(fd);
+            extension_buffer_sizes->emplace_back(dataSize);
+            if (fd >= 0 && dataSize > 0)
+                extension_buffers->emplace_back(mmap(nullptr, dataSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd,0));
+            else
+                extension_buffers->emplace_back(nullptr);
+            return extension_buffers->at(extension_buffers->size() - 1);
+        }
 
-    void* addExtensionFD(int fd, int dataSize) {
-        extension_fds->emplace_back(fd);
-        extension_buffer_sizes->emplace_back(dataSize);
-        if (fd >= 0 && dataSize > 0)
-            extension_buffers->emplace_back(mmap(nullptr, dataSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd,0));
-        else
-            extension_buffers->emplace_back(nullptr);
-        return extension_buffers->at(extension_buffers->size() - 1);
-    }
-
-    // So far it is used only by aap_client_as_plugin.
-    aap_buffer_t* getAudioPluginBuffer() {
-        assert(port_buffer); // make sure to call allocate*Buffer() first.
-        return port_buffer->toPublicApi();
-    }
-};
+        // So far it is used only by aap_client_as_plugin.
+        aap_buffer_t* getAudioPluginBuffer() {
+            assert(port_buffer); // make sure to call allocate*Buffer() first.
+            return port_buffer->toPublicApi();
+        }
+    };
 
     class ClientPluginSharedMemoryStore : public PluginSharedMemoryStore {
     public:
