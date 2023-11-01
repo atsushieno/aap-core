5f1b786dbf14dab409a77d4d3a91b396f49fb16e
Atsushi Eno <atsushieno@gmail.com>
2023-11-02 00:28:42 +0900

    Add midi AAPXS v2.

---
+++ androidaudioplugin/src/main/cpp/core/aapxs/midi-aapxs.cpp
@@ -0,0 +1,49 @@
+
+#include "midi-aapxs.h"
+#include "../AAPJniFacade.h"
+
+#if USE_AAPXS_V2
+int32_t getMidiSettingsFromLocalConfig(std::string pluginId) {
+    return aap::AAPJniFacade::getInstance()->getMidiSettingsFromLocalConfig(pluginId);
+}
+#else
+extern int32_t getMidiSettingsFromLocalConfig(std::string pluginId);
+#endif
+
+void aap::xs::AAPXSDefinition_State::aapxs_midi_process_incoming_plugin_aapxs_request(
+        struct AAPXSDefinition *feature, AAPXSRecipientInstance *aapxsInstance,
+        AndroidAudioPlugin *plugin, AAPXSRequestContext *request) {
+    switch (request->opcode) {
+        case OPCODE_GET_MAPPING_POLICY:
+            auto len = *(int32_t *) request->serialization->data;
+            assert(len < AAP_MAX_PLUGIN_ID_SIZE);
+            char *pluginId = (char *) calloc(len + 1, 1);
+            strncpy(pluginId, (const char *) ((int32_t *) request->serialization->data + 1), len);
+            *((int32_t *) request->serialization->data) = getMidiSettingsFromLocalConfig(pluginId);
+            break;
+    }
+}
+
+void aap::xs::AAPXSDefinition_State::aapxs_midi_process_incoming_host_aapxs_request(
+        struct AAPXSDefinition *feature, AAPXSRecipientInstance *aapxsInstance,
+        AndroidAudioPluginHost *host, AAPXSRequestContext *request) {
+    throw std::runtime_error("There is no MIDI host extension");
+}
+
+void aap::xs::AAPXSDefinition_State::aapxs_midi_process_incoming_plugin_aapxs_reply(
+        struct AAPXSDefinition *feature, AAPXSInitiatorInstance *aapxsInstance,
+        AndroidAudioPlugin *plugin, AAPXSRequestContext *request) {
+    if (request->callback != nullptr)
+        request->callback(request->callback_user_data, plugin, request->request_id);
+}
+
+void aap::xs::AAPXSDefinition_State::aapxs_midi_process_incoming_host_aapxs_reply(
+        struct AAPXSDefinition *feature, AAPXSInitiatorInstance *aapxsInstance,
+        AndroidAudioPluginHost *host, AAPXSRequestContext *request) {
+    if (request->callback != nullptr)
+        request->callback(request->callback_user_data, host, request->request_id);
+}
+
+int32_t aap::xs::MidiClientAAPXS::getMidiMappingPolicy() {
+    return callTypedFunctionSynchronously<int32_t>(OPCODE_GET_MAPPING_POLICY);
+}
---
+++ androidaudioplugin/src/main/cpp/core/aapxs/midi-aapxs.h
@@ -0,0 +1,78 @@
+
+
+#ifndef AAP_CORE_MIDI_AAPXS_H
+#define AAP_CORE_MIDI_AAPXS_H
+
+#include <functional>
+#include <future>
+#include "aap/unstable/aapxs-vnext.h"
+#include "aap/ext/midi.h"
+#include "aap/core/aapxs/aapxs-runtime.h"
+
+// plugin extension opcodes
+const int32_t OPCODE_GET_MAPPING_POLICY = 0;
+
+// host extension opcodes
+// ... nothing?
+
+const int32_t MIDI_SHARED_MEMORY_SIZE = AAP_MAX_PLUGIN_ID_SIZE + sizeof(int32_t);
+
+namespace aap::xs {
+    class AAPXSDefinition_State : public AAPXSDefinitionWrapper {
+
+        static void aapxs_midi_process_incoming_plugin_aapxs_request(
+                struct AAPXSDefinition* feature,
+                AAPXSRecipientInstance* aapxsInstance,
+                AndroidAudioPlugin* plugin,
+                AAPXSRequestContext* request);
+        static void aapxs_midi_process_incoming_host_aapxs_request(
+                struct AAPXSDefinition* feature,
+                AAPXSRecipientInstance* aapxsInstance,
+                AndroidAudioPluginHost* host,
+                AAPXSRequestContext* request);
+        static void aapxs_midi_process_incoming_plugin_aapxs_reply(
+                struct AAPXSDefinition* feature,
+                AAPXSInitiatorInstance* aapxsInstance,
+                AndroidAudioPlugin* plugin,
+                AAPXSRequestContext* request);
+        static void aapxs_midi_process_incoming_host_aapxs_reply(
+                struct AAPXSDefinition* feature,
+                AAPXSInitiatorInstance* aapxsInstance,
+                AndroidAudioPluginHost* host,
+                AAPXSRequestContext* request);
+
+        AAPXSDefinition aapxs_midi{AAP_MIDI_EXTENSION_URI,
+                                    this,
+                                    MIDI_SHARED_MEMORY_SIZE,
+                                    aapxs_midi_process_incoming_plugin_aapxs_request,
+                                    aapxs_midi_process_incoming_host_aapxs_request,
+                                    aapxs_midi_process_incoming_plugin_aapxs_reply,
+                                    aapxs_midi_process_incoming_host_aapxs_reply
+        };
+
+    public:
+        AAPXSDefinition& asPublic() override {
+            return aapxs_midi;
+        }
+    };
+
+    class MidiClientAAPXS : public TypedAAPXS {
+    public:
+        MidiClientAAPXS(AAPXSInitiatorInstance* initiatorInstance, AAPXSSerializationContext* serialization)
+                : TypedAAPXS(AAP_MIDI_EXTENSION_URI, initiatorInstance, serialization) {
+        }
+
+        int32_t getMidiMappingPolicy();
+    };
+
+    class MidiServiceAAPXS : public TypedAAPXS {
+    public:
+        MidiServiceAAPXS(AAPXSInitiatorInstance* initiatorInstance, AAPXSSerializationContext* serialization)
+                : TypedAAPXS(AAP_MIDI_EXTENSION_URI, initiatorInstance, serialization) {
+        }
+
+        // nothing?
+    };
+}
+
+#endif //AAP_CORE_MIDI_AAPXS_H
